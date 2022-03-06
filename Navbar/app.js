f012d6b9b4dcd45775ec22ababf4d5121ebd06e2
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-03-05 23:17:06 -0600

    completed app.js logic and tested functionality, its a go.

--- Navbar/app.js
+++ Navbar/app.js
@@ -1,5 +1,21 @@
 // classList - shows/gets all classes
-// contains - checks classList for specific class
-// add - add class
-// remove - remove class
-// toggle - toggles class
+// contains - checks classList for specific class (method)
+// add - add class (add method)
+// remove - remove class (remove method)
+// toggle - toggles class (will do this the long way for understanding and then use one liner)
+
+const navToggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.links');
+
+navToggle.addEventListener('click', function (){
+   //console.log(links.classList); //shows in console
+    //console.log(links.classList.contains("random"));// passing class we are checking for --> got response of false
+    //console.log(links.classList.contains("links")); // shows true in console log
+    // if (links.classList.contains('show-links')){
+    //     links.classList.remove('show-links');
+    // }
+    // else {
+    //     links.classList.add("show-links");
+    // }
+    links.classList.toggle("show-links");
+});
