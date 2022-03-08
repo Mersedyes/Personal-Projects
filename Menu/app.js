84f64f85c0752c06620ce0f466e3019330ac0d1f
Mersedyes Livingston <mersedyes.livingston@gmail.com>
2022-03-07 21:04:33 -0600

    refactored code in app.js to make dynamic by accessing the variables running the array through a function

--- Menu/app.js
+++ Menu/app.js
@@ -79,7 +79,11 @@ const sectionCenter = document.querySelector('.section-center');
 
 window.addEventListener('DOMContentLoaded', function (){
     //console.log("shake and bake");
-    let displayMenu = menu.map(function (item){
+    displayMenuItems(menu)
+});
+
+function displayMenuItems(menuItems){
+    let displayMenu = menuItems.map(function (item){
         //console.log(item);
 
         //return `<h1>${item.title}</h>`;
@@ -98,5 +102,6 @@ window.addEventListener('DOMContentLoaded', function (){
         </article>`;
     });
     displayMenu = displayMenu.join("");
+
     sectionCenter.innerHTML = displayMenu;
-});
+}
